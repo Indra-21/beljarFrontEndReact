e0d5f8ff1a3573ddf841f8b5f8f8d06165a24a41
Indra Saputra <indrasaputratra21@gmail.com>
2021-11-08 00:17:12 +0700

    belajarReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from "./component/AppBar";
+import Students from './component/Students';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className = "App" >
+        <AppBar/>
+        <Students/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/component/Students.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Container, Paper, Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme)=>({
+    root:{
+        '& > *':{
+            margin:theme.spacing(1),
+        },
+    },
+}));
+
+export default function Students() {
+    const paperStyle = {padding:'150px, 50px', width:600, margin:'50px auto'}
+    const [nama, setNama] =useState('')
+    const [alamat, setAlamat] =useState('')
+    const [student, setStudent] =useState([])
+    const classes = useStyles();
+
+    const handleClick=(e)=>{
+        e.preventDefault()
+        const students={nama, alamat}
+        console.log(students)
+        fetch("http://localhost:8081/student/add",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(students)
+
+        }).then(()=>{
+            console.log("New Student Add")
+        })
+    }
+
+    useEffect(()=>{
+        fetch("http://localhost:8081/student/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setStudent(result);
+        })
+    },[])
+
+  return (
+
+    <Container>
+        <Paper elevation={3} style={paperStyle}>
+            <h1 style={{ color:"blue" }}><u>Add Student</u></h1>
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="Students nama" variant="outlined" fullWidth 
+      value={nama} 
+      onChange={(e)=>setNama(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Students alamat" variant="outlined" fullWidth 
+      value={alamat}
+      onChange={(e)=>setAlamat(e.target.value)}
+      />
+      <Button variant="contained" color="secondary" onClick={handleClick}>Submit</Button>
+      </form>
+      </Paper>
+      <hi>Student</hi>
+
+      <Paper elevation={3} style={paperStyle}>
+          {
+              student.map(students=>(
+                <Paper elevation={6} style={{ margin:"10px", padding:"15px", textAlign:"left" }} key={students.id}>
+                Id:{students.id}<br/>
+                Nama:{students.nama}<br/>
+                alamat:{students.alamat}
+                </Paper>
+                ))
+          }
+          </Paper>
+    </Container>
+  );
+}
